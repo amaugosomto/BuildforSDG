0abebf3006bab148c8560ec1a24a2a7986a07af7
somto amaugo <amaugosomto@gmail.com>
2020-04-09 16:36:58 +0100

    finished challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,38 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  return {
+    data: {},
+    impact: {
+      currentlyInfected: data.reportedCases * 10,
+      infectionsByRequestedTime: Math.trunc((data.reportedCases * 10) * getInfectionsByRequestedTime(data.periodType, data.timeToElapse))
+    },
+    severeImpact: {
+      currentlyInfected: data.reportedCases * 50,
+      infectionsByRequestedTime: Math.trunc((data.reportedCases * 50) * getInfectionsByRequestedTime(data.periodType, data.timeToElapse))
+    }
+  };
+};
+
+const getInfectionsByRequestedTime = (periodType, timeToElapse ) => {
+  let noOfDays = 0;
+
+  switch (periodType) {
+    case "days":
+      noOfDays = timeToElapse;  
+    break;
+  
+    case "weeks":
+      noOfDays = timeToElapse * 7;
+    break;
+  
+    case "months":
+      noOfDays = timeToElapse * 30;
+    break;
+
+    default:
+      break;
+  }
+
+  return Math.pow(noOfDays, 10);
+}
 
 export default covid19ImpactEstimator;
