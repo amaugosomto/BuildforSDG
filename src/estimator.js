921fe2537ba629e9f86804513076660d52631e84
somto amaugo <amaugosomto@gmail.com>
2020-04-11 11:02:46 +0100

    logic change

--- src/estimator.js
+++ src/estimator.js
@@ -37,8 +37,10 @@ const covid19ImpactEstimator = (data) => {
   );
   const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
   const severeCasesForICUByRequestedTime = Math.trunc(severeInfectionsByRequestedTime * 0.05);
-  const casesForVentilatorsByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
-  const severeCasesForVentilatorsByRequestedTime = severeInfectionsByRequestedTime * 0.02;
+  const casesForVentilatorsByRequestedTime = Math.trunc(
+    casesForICUByRequestedTime * 0.02
+  );
+  const severeCasesForVentilatorsByRequestedTime = severeCasesForICUByRequestedTime * 0.02;
   const dollarsInFlight = Math.round(((infectionsByRequestedTime
     * 0.65 * data.region.avgDailyIncomeInUSD) * noOfDays) * 100) / 100;
   const severeDollarsInFlight = Math.round(((severeInfectionsByRequestedTime
